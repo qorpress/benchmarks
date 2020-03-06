1ef82475e3e8fe90b0cdb8076cee85e3b73ed5d2
x0rzkov <x0rzkov@protonmail.com>
2020-03-06 09:30:25 +0100

    initial commit

---
+++ go-template/golang/navigation.go
@@ -0,0 +1,22 @@
+package golang
+
+import (
+	"html"
+
+	"github.com/valyala/bytebufferpool"
+
+	"github.com/qorpress/benchmarks/go-template/model"
+)
+
+func Navigation(bb *bytebufferpool.ByteBuffer, nav []*model.Navigation) {
+	bb.WriteString(`<ul class="navigation">`)
+	for _, item := range nav {
+		bb.WriteString(`<li><a href="`)
+		bb.WriteString(html.EscapeString(item.Link))
+		bb.WriteString(`">`)
+		bb.WriteString(html.EscapeString(item.Item))
+		bb.WriteString(`</a></li>`)
+
+	}
+	bb.WriteString(`</ul>`)
+}
